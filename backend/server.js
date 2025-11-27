b263c3faaec48d5d55036d3a98eef8d665b18b77
Luis Feliz <Luisfeliz@gmail.com>
2025-11-26 19:58:40 -0500

    updates for render-14

--- backend/server.js
+++ backend/server.js
@@ -105,7 +105,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
